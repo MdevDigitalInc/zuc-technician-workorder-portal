2c2b35a8d583bb7dc271f780615078345073d161
Lucas <l.moreira@live.ca>
2017-03-22 19:49:57 -0400

    Added Missing files

---
+++ portal-app-source/src/components/shared/mobile-navigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="mdev-mobile-nav u-hidden-desktop" aria-role="navigation" aria-hidden="true" role="navigation">
+    <div class="mdev-main-wrapper flex flex-nowrap flex-hor-between flex-vert-center">
+      <!-- -->
+      <navlinks></navlinks>  
+    </div>
+  </div>
+</template>
+
+<script>
+  //Import Nav Links
+  import NavLinks from './navlinks.vue';
+  
+  export default {
+    name: "mobilenav",
+
+    components: {
+      'navlinks' : NavLinks
+    }
+  };
+</script>
+
+<style lang="scss">
+
+  /*--------------------------------------*/
+  /* Lean Import for Components           */
+  /*--------------------------------------*/
+
+  /* Disable because they are already linted */
+  /* stylelint-disable */
+  @import '../../assets/styles/component-lean-main.scss';
+  /* stylelint-enable */
+
+  /*--------------------------------------*/
+  /* Main Component Styles                */
+  /*--------------------------------------*/
+  
+  .mdev-mobile-nav {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    background: $zucora-blue;
+    padding: $small-spacing 0;
+    left: 0;
+
+    .mdev-main-nav-links {
+      width: 100%;
+      justify-content: space-between;
+      margin: 0;
+    }
+
+    .mdev-icon {
+      margin: 0 auto $medium-spacing auto;
+    }
+
+    .mdev-nav-link {
+      width: 25%;
+    }
+  }
+
+  
+</style>
---
+++ portal-app-source/src/components/shared/navlinks.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="mdev-main-nav-links flex flex-nowrap flex-hor-end flex-vert-center u-capitalize">
+    <router-link 
+      v-for="link in links" 
+      :to="link.route"
+      class ="mdev-nav-link flex flex-hor-between flex-vert-center u-bold"
+      active-class="--active" 
+      :title="link.linkTitle" 
+      aria-role="menuitem" 
+      exact>
+        <i :class="['mdev-icon --size-s', link.iconClass ]"></i>
+        <span>{{ link.linkName  }}</span>
+   </router-link> 
+  </div>
+</template>
+
+
+
+<script>
+  export default{
+    // <router-link> element is a custom element derived from vue-router. use :to - to bind. 
+    name: "navLinks",
+
+    data: function(){
+      return{
+        // Refer to routes.js file for available routes.
+        links: [
+          {
+            linkName: 'Work Orders',
+            linkTitle: 'Available Work Orders',
+            iconClass: '--workorder-icon',
+            route: '/dashboard/list'
+          },
+          {
+            linkName: 'Billing',
+            iconClass: '--billing-icon',
+            linkTitle: 'Billing Report',
+            route: '/dashboard/billing'
+          },
+          {
+            linkName: 'Contact',
+            iconClass: '--contact-icon',
+            linkTitle: 'Contact Information',
+            route: '#'
+          },
+          {
+            linkName: 'User',
+            iconClass: '--user-icon',
+            linkTitle: 'User Menu',
+            route: '#'
+          }
+        ]
+      };
+    }
+  };
+
+</script>
+
+
+
+<style lang="scss">
+
+  /*--------------------------------------*/
+  /* Lean Import for Components           */
+  /*--------------------------------------*/
+
+  /* Disable because they are already linted */
+  /* stylelint-disable */
+  @import '../../assets/styles/component-lean-main.scss';
+  /* stylelint-enable */
+
+  .mdev-main-nav-links {
+    color: $white;
+    font-weight: $body-weight;
+    
+    @media screen and ('$tablet-up-comp') {
+      color: $charcoal-grey;
+    }
+  }
+
+  .mdev-nav-link {
+    transition: all, .3s;
+    flex-wrap: wrap;
+    text-align: center;
+
+    @media screen and ('$tablet-up-comp') {
+      flex-wrap: nowrap;
+      text-align: left;
+    }
+
+    &:hover {
+      color: $zucora-green;
+    }
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    span {
+      width: 100%;
+
+      @media screen and ('$tablet-up-comp') {
+        width: auto;
+      }
+    }
+  }
+
+  .--active {
+    color: $zucora-green;
+  }
+  /*--------------------------------------*/
+
+</style>
